7c335b0529498ccf61ca137f4b42420dc3a3b763
Ballet <dai-lab@mit.edu>
2019-05-07 20:12:36 +0000

    Pruning Redundant Features: 05.04, 05.05, 08.04

--- ames/features/contrib/user_05/feature_05.py
+++
@@ -1,7 +0,0 @@
-from ballet import Feature
-from sklearn.preprocessing import OneHotEncoder
-
-input = ["Overall Cond"]
-transformer = OneHotEncoder()
-name = "Overall Cond Encoded"
-feature = Feature(input=input, transformer=transformer, name=name)
--- ames/features/contrib/user_08/feature_04.py
+++
@@ -1,7 +0,0 @@
-from ballet import Feature
-from ballet.eng import NullFiller
-
-input = "Mas Vnr Area"
-transformer = NullFiller()
-name = "Masonry Veneer Area Fill"
-feature = Feature(input=input, transformer=transformer, name=name)
